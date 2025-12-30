d14f75335d413c856da70bd0fa542a3252934f98
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-30 23:15:30 +0530

    d

--- src.js
+++ src.js
@@ -6,3 +6,11 @@ let countItem = items.length;
 let active = 1;
 let other_1 = null;
 let other_2 = null;
+next.onclick = () => {
+    carousel.classList.remove('prev');
+    carousel.classList.add('next');
+    active =active + 1 >= countItem ? 0 : active + 1;
+    other_1 =active - 1 < 0 ? countItem -1 : active - 1;
+    other_2 = active + 1 >= countItem ? 0 : active + 1;
+    changeSlider();
+}
