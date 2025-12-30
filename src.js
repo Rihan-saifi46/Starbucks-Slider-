68c3f8f2dc67492d3be96a16026319b81c32807b
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-30 23:15:16 +0530

    d

--- src.js
+++ src.js
@@ -3,3 +3,6 @@ let prev = document.getElementById('prev');
 let carousel = document.querySelector('.carousel');
 let items = document.querySelectorAll('.carousel .item');
 let countItem = items.length;
+let active = 1;
+let other_1 = null;
+let other_2 = null;
